be4fadf03596d0923df32afd81ec34ebfa1bce27
ZhengTian <38405540+tangzt96@users.noreply.github.com>
2021-09-27 16:30:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+}
+
+let solveRect = (l,b) => {
+    console.log("Solving for rectangle: " + l + " + " +  b  )
+
+    if (l<=0 || b <=0){
+        console.log("Rectangle dimension cannot be zero")
+    }
+    else{
+        console.log("area of rectangle = " , + rect.area(l,b));
+        console.log("perimeter of rectangle = " , + rect.perimeter(l,b));
+
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-1,3);
