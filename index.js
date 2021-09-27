442796f6c28b29a57aab53628dad72f7f630d49a
ZhengTian <38405540+tangzt96@users.noreply.github.com>
2021-09-27 17:20:18 +0800

    simple node module

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var rect = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-}
+var rect = require('./rectangle')
 
 let solveRect = (l,b) => {
     console.log("Solving for rectangle: " + l + " + " +  b  )
